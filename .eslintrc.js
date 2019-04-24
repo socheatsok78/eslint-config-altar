37ce2e12ca70838040bc6ac4ae811e909bb8a76e
Socheat <socheatsok78@gmail.com>
2019-04-24 16:25:07 +0700

    Revert "Update .eslintrc.js, remove any trace of react & react-hook"
    This reverts commit 0c8a14bcbd66949e568aa5fea6133ca274538852.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,9 @@
 module.exports = {
     "extends": [
         "airbnb",
-        "plugin:vue/recommended",
-        "prettier/vue"
-    ],
-    "plugins": [
         "prettier",
+        "prettier/react",
+        "plugin:vue/essential"
     ],
     "parserOptions": {
         "ecmaVersion": 2017,
@@ -116,6 +114,26 @@ module.exports = {
             }
         ],
 
+        /**
+         * React ESLint Plugins
+         */
+        "react/display-name": 1,
+        "react/no-array-index-key": 0,
+        "react/react-in-jsx-scope": 0,
+        "react/prefer-stateless-function": 0,
+        "react/forbid-prop-types": 0,
+        "react/no-unescaped-entities": 0,
+        "jsx-a11y/accessible-emoji": 0,
+        "react/require-default-props": 0,
+        "react/jsx-filename-extension": [
+            1,
+            {
+                "extensions": [
+                    ".js",
+                    ".jsx"
+                ]
+            }
+        ],
         "jsx-a11y/href-no-hash": "off",
         "jsx-a11y/anchor-is-valid": [
             "warn",
@@ -125,13 +143,15 @@ module.exports = {
                 ]
             }
         ],
+        "react-hooks/rules-of-hooks": "error",
+        "react-hooks/exhaustive-deps": "warn",
 
         /**
          * Vue ESLint Plugins
          */
         "vue/html-indent": [
             "error",
-            2,
+            4,
             {
                 "attribute": 1,
                 "baseIndent": 1,
@@ -140,7 +160,6 @@ module.exports = {
                 "ignores": []
             }
         ],
-        "vue/singleline-html-element-content-newline": "off",
         "vue/html-closing-bracket-newline": [
             "error",
             {
@@ -148,5 +167,10 @@ module.exports = {
                 "multiline": "always"
             }
         ]
-    }
+    },
+    "plugins": [
+        "html",
+        "prettier",
+        "react-hooks"
+    ]
 }
